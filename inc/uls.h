79ad18997cc9b8b9b35fe5b08200d37cad5b96d1
mmkost <41927762+mmkost@users.noreply.github.com>
2020-02-12 19:27:08 +0200

    Add files via upload

--- inc/uls.h
+++ inc/uls.h
@@ -1,11 +1,15 @@
 #ifndef ULS_H
 #define ULS_H
 
-#include "libmx.h"
+#include "libmx/inc/libmx.h"
 #include <stdio.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 //int main(void);
+t_list *mx_sort(t_list *lst);
+int mx_biggest(t_list *p);
+void mx_multcol(int ws, int length, t_list *names);
 
 #endif
