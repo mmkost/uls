13a8e40312d704b2e7fddf25d8655eb9b128d8ef
Mariya Kostych <mkostych@e2r14p9.unit.ua>
2020-03-06 19:07:05 +0200

    parse

--- src/mx_parse.c
+++ src/mx_parse.c
@@ -1,8 +1,40 @@
 #include "uls.h"
 
+static int len_record(int argc, char **argv) {
+    int size = 0;
+
+    for (int i = 1; i < argc && argv[i][0] == '-'; i++)
+        if (argv[i][0] == '-' && argv[i][1] != '\0')
+            for (int j = 1; argv[i][j] != '\0'; j++)
+                if (argv[i][0] == '-' && argv[i][1] != '-')
+                	size = size + mx_strlen(argv[i]) - 1;
+    return size;
+}
+
+static char *record(int argc, char **argv) {
+    char *str = NULL;
+    int size = 0;
+    int k = 0;
+
+    size = len_record(argc, argv);
+    str = mx_strnew(size);
+    for (int i = 1; i < argc && argv[i][0] == '-'; i++)
+        if (argv[i][0] == '-' && argv[i][1] != '\0')
+            for (int j = 1; argv[i][j] != '\0'; j++)
+                if (argv[i][0] == '-') {
+                	if (mx_strcmp(argv[i], "--") == 0) 
+                		return str;
+                    else
+                        str[k++] = argv[i][j];
+                }
+    return str;
+} 
+
 void mx_parse(int argc, char **argv) {
-    char *str = mx_strnew(2);
-    str = "1l";
+    char *str = mx_strnew(11);
+    //char str[11];
+    str = "1lmCSturafA";
+    char *true_str = NULL;
 
     for (int i = 1; i < argc && argv[i][0] == '-'; i++) {
         if (argv[i][0] == '-' && argv[i][1] != '\0') {
@@ -10,10 +42,14 @@ void mx_parse(int argc, char **argv) {
                 if (mx_get_char_index(str, argv[i][j]) == -1) {
                         write(2, "uls: illegal option -- ", 23);
                         write(2, &argv[i][j], 1);
-                        write(2, "\nusage: uls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]\n", 64);
+                        write(2, "\nusage: uls [-ACSaflmrtu1] [file ...]\n", 38);
                         exit(1);
                     }
+                else
+                    true_str = record(argc, argv);    
             }
         }
     }
+    mx_printstr(true_str);
+    mx_printchar('\n');
 }
